78e2e86458f0c382f182cbdac7da495b52bbbd52
ztlab111 <dineshp@zignuts.com>
2023-05-08 16:10:08 +0530

    create class

--- app/src/main/java/com/example/fooddonation/ui/dashboard/DashBoardFragment.kt
+++ app/src/main/java/com/example/fooddonation/ui/dashboard/DashBoardFragment.kt
@@ -1,4 +1,4 @@
-package com.example.fooddonation.ui.desboard
+package com.example.fooddonation.ui.dashboard
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -35,7 +35,7 @@ class DashBoardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dash_board, container, false)
+        return inflater.inflate(R.layout.fragment_dash_board2, container, false)
     }
 
     companion object {
